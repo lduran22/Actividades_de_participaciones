18793fd009a30220b9d7cc3f972157c67d5c9685
Luis Duran <luisangeldurantoscano@outlook.com>
2024-08-04 22:40:59 -0500

    se agrega punto 1 y 2 de actividad de participacion 1

---
+++ Actividad_de_participacion1/area.py
@@ -0,0 +1,9 @@
+
+def calcular_area_circulo(radio): 
+    area = 3.14 * (radio ** 2)
+    return area
+
+
+radio = 5
+area = calcular_area_circulo(radio)
+print(f"El área de un círculo con radio {radio} es {area}")
---
+++ Actividad_de_participacion1/nombre_edad.py
@@ -0,0 +1,5 @@
+
+nombre = input("Por favor, ingresa tu nombre: ")
+edad = input("Por favor, ingresa tu edad: ")
+
+print(f"Tu nombre es {nombre} y tienes {edad} años.")
